b0c26eea481b9542ff1d704a0d69a2b0ae8a726e
Jandave0 <185737041+Jandave0@users.noreply.github.com>
2026-04-07 10:29:37 +0800

    implement scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,18 @@
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all';
-import React from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+    const videoref = useRef();
+    const videoTimelineRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
     useGSAP(() => {
-        const heroSplit = new SplitText('.title',  {type: ' words, chars'});
-        const paragraphSplit = new SplitText('.subtitle',  {type: 'lines'});
+        const heroSplit = new SplitText('.title', { type: ' words, chars' });
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
 
-        heroSplit.chars.forEach((char) =>char.classList.add('text-gradient'));
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
 
         gsap.from(heroSplit.chars, {
             yPercent: 100,
@@ -27,45 +31,87 @@ const Hero = () => {
         });
 
         gsap.timeline({
-            scrollTrigger:{
+            scrollTrigger: {
                 trigger: '#hero',
                 start: 'top top',
                 end: 'bottom top',
                 scrub: true,
             }
         })
-        .to('.right-leaf', {y:200 }, 0)
-        .to('.left-leaf', {y: -200}, 0)
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'center 60%' : 'center 50%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+                // onEnter: () => videoref.current.play(),
+                // onEnterBack: () => videoref.current.play(),
+                // onLeave: () => videoref.current.pause(),
+                // onLeaveBack: () => videoref.current.pause(),
+            }
+        });
+
+        videoref.current.onloadedmetadata = () => {
+            tl.to(videoref.current, {
+                currentTime: videoref.current.duration,
+
+            })
+        }
+
+        // videoTimelineRef.current.to(videoref.current, {
+        //     y: 150,
+        //     scale: 0.9,
+        //     opacity: 0,
+        //     ease: "none"
+        // });
+
     }, []);
-  return (
-    <>
-    <section id='hero' className='noisy'>
-        <h1 className='title'>
-            MOJOJOJO
-        </h1>
-        <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf'/>
-        <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf'/>
-        <div className='body'>
-            <div className='content'>
-            <div className='space-y-5 hidden md:block'>
-                <p>
-                    Experience the art of mixology 
-                </p>
-                <p className='subtitle'>
-                    Unleash your inner bartender with <br />our exquisite cocktail recipes
-                </p>
+    return (
+        <>
+            <section id='hero' className='noisy'>
+                <h1 className='title'>
+                    MOJOJOJO
+                </h1>
+                <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
+                <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
+                <div className='body'>
+                    <div className='content'>
+                        <div className='space-y-5 hidden md:block'>
+                            <p>
+                                Experience the art of mixology
+                            </p>
+                            <p className='subtitle'>
+                                Unleash your inner bartender with <br />our exquisite cocktail recipes
+                            </p>
+                        </div>
+                        <div className='view-cocktails'>
+                            <p className='subtitle'>
+                                every cocktail you can imagine, and more all in one place. That's our promise to you. youll find the most delicious, unique, and creative cocktails on our website.
+                            </p>
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className='video absolute inset-0'>
+                <video
+                    ref={videoref}
+                    src='/videos/output.mp4'
+                    muted
+                    playsInline
+                    preload='auto'
+
+                />
             </div>
-            <div className='view-cocktails'>
-                <p className='subtitle'>
-                    every cocktail you can imagine, and more all in one place. That's our promise to you. youll find the most delicious, unique, and creative cocktails on our website. 
-                </p>
-                <a href="#cocktails">View Cocktails</a>
-            </div>
-            </div>
-        </div>
-    </section>
-    </>
-  )
+        </>
+    )
 }
 
 export default Hero
